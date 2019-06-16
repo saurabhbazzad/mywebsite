035313f3a7cf8d3682af6858297c73ea9161d904
Saurabh <saurabhbazzad@gmail.com>
2019-06-16 17:06:43 +0530

    added typed library

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+    new Typed('#typed',{
+        strings:["Saurabh Singh Bazzad","Full Stack Developer","Coffee Lover","Pro Meme Maker"],
+        typeSpeed:100,
+        backSpeed:50,
+        backDelay:2000,
+        loop:true,
+        smartBackspace:true,
+        callback:function(){}
+    })
+}
